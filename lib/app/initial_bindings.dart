7fef7d7b8a75d3397aa8415566ec42be1bdfe79f
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2025-01-23 03:23:39 +0530

    Created initial_bindings.dart.

---
+++ lib/app/initial_bindings.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+import 'data/repository_impl/login_repository_impl.dart';
+import 'domain/repository/login_repository.dart';
+import 'app_commons/network/app_dio.dart';
+import 'data/remote/login_rest_client.dart';
+
+class InitialBindings {
+  InitialBindings() {
+    Get.put<LoginRestClient>(LoginRestClient(AppDio().getDio()));
+    Get.put<LoginRepository>(LoginRepositoryImpl());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:enhanzer_sample/app/initial_bindings.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  InitialBindings();
+
   runApp(
     GetMaterialApp(
       title: "Application",
