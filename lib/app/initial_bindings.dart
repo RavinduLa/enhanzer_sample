78cd4d150da898bfe07d7d685fa13ae7b55f8121
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2025-01-23 15:21:19 +0530

    Added sqflite_service to initial bindings.

--- lib/app/initial_bindings.dart
+++ lib/app/initial_bindings.dart
@@ -1,3 +1,4 @@
+import 'package:enhanzer_sample/app/data/local/sqflite_service.dart';
 import 'package:get/get.dart';
 
 import 'data/repository_impl/login_repository_impl.dart';
@@ -7,6 +8,7 @@ import 'data/remote/login_rest_client.dart';
 
 class InitialBindings {
   InitialBindings() {
+    Get.putAsync(() => SQFLiteService().init());
     Get.put<LoginRestClient>(LoginRestClient(AppDio().getDio()));
     Get.put<LoginRepository>(LoginRepositoryImpl());
   }
