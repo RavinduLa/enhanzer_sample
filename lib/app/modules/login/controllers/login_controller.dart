49568756d3656dbe30d6d8997720b321e95abb31
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2025-01-23 12:37:13 +0530

    Data binding for login view and controller.

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,107 @@
+import 'package:either_dart/either.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+import '../../../app_commons/common_widgets/snackbars/failure_snackbar.dart';
+import '../../../app_commons/common_widgets/snackbars/success_snackbar.dart';
+import '../../../app_commons/exceptions/login_exception.dart';
+import '../../../domain/repository/login_repository.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  final LoginRepository loginRepository = Get.find();
 
-  final count = 0.obs;
+  RxBool isLoading = false.obs;
+  final loginFormKey = GlobalKey<FormState>();
+  final companyCodeController = TextEditingController();
+  final passwordController = TextEditingController();
+  final RxString loadingString = "Loading".obs;
+  final RxBool passwordObscured = true.obs;
+
   @override
   void onInit() {
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void onLoginTapped() async {
+    bool formValid = validateForm();
+    if (formValid) {
+      isLoading.value = true;
+      await _loginRemoteCall();
+      isLoading.value = false;
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  //Validate the form
+  bool validateForm() {
+    return loginFormKey.currentState!.validate();
   }
 
-  void increment() => count.value++;
+  //Change the visibility status of the password field
+  void flipPasswordFieldVisibility() {
+    passwordObscured.value = !passwordObscured.value;
+  }
+
+  Future<void> _loginRemoteCall() async {
+    //Make call
+    Either<LoginException, bool> result = await loginRepository.login(
+      companyCodeController.text.trim(),
+      passwordController.text.trim(),
+    );
+
+    //Handle result
+    result.fold((left) {
+      //Handle error
+      _handleLoginError(left);
+    }, (right) {
+      //Handle success scenario
+      _handleSuccess();
+    });
+  }
+
+  //Handle success scenarios for login
+  void _handleSuccess() async {
+    _showSuccessSnackBar(
+      "Success",
+      "Your username and password are correct.",
+    );
+  }
+
+  //Handle the error scenarios for login
+  void _handleLoginError(LoginException exception) async {
+    if (exception.errorType == LoginExceptionType.invalidCredentials) {
+      _showFailureSnackBar(
+        "Invalid credentials",
+        "Either your username or password is invalid",
+        null,
+      );
+    } else {
+      _showFailureSnackBar(
+        "Error",
+        "An unknown error has occurred. Please try again and make sure you are connected to the internet.",
+        null,
+      );
+    }
+  }
+
+  Future<void> _showFailureSnackBar(
+    String title,
+    String message,
+    Function(GetSnackBar)? onTap,
+  ) async {
+    FailureSnackBar(
+      title: title,
+      message: message,
+      onTap: onTap,
+    ).show();
+  }
+
+  Future<void> _showSuccessSnackBar(
+    String title,
+    String message,
+  ) async {
+    SuccessSnackBar(
+      title: title,
+      message: message,
+    ).show();
+  }
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -5,7 +5,9 @@ import 'package:get/get.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({super.key});
+  LoginView({super.key});
+  final LoginController loginController = Get.put(LoginController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,10 +15,104 @@ class LoginView extends GetView<LoginController> {
         title: const Text('Login'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: Form(
+        key: loginController.loginFormKey,
+        child: Obx(
+          () => loginController.isLoading.value
+              ? Center(
+                  child: Text('Loading'),
+                )
+              : SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        controller: loginController.companyCodeController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          hintText: 'youeemail@email.com',
+                          labelText: 'Username',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            //Handle empty field
+                            return "Please enter the username. This can be your company code as well.";
+                          } else {
+                            //Regex for email
+                            final bool emailValid = RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value);
+                            if (!emailValid) {
+                              return "Please enter a valid email";
+                            }
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          loginController.validateForm();
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Obx(
+                        () => TextFormField(
+                          controller: loginController.passwordController,
+                          obscureText: loginController.passwordObscured.value,
+                          decoration: InputDecoration(
+                            hintText: '********',
+                            labelText: 'Password',
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                loginController.flipPasswordFieldVisibility();
+                              },
+                              icon: loginController.passwordObscured.value
+                                  ? const Icon(Icons.visibility_off)
+                                  : const Icon(Icons.visibility),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              //Handle empty field
+                              return "Please provide a password";
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            loginController.validateForm();
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                loginController.onLoginTapped();
+                              },
+                              child: const Text('Sign In'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
         ),
       ),
     );
